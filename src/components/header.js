5ae040b0e8c5233b07074d02f24e2962f2e9da00
aaronhung3 <yulehung09@gmail.com>
2025-02-16 05:15:01 -0500

    fix download

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import './header.css'
 function Header() {
 
     const onButtonClick = () => {
-        const pdfUrl = process.env.PUBLIC_URL ? process.env.PUBLIC_URL + "/aaronhung_resume.pdf" : "/aaronhung_resume.pdf";
+        const pdfUrl = "https://aaronhung3w.github.io/aaronhung_resume.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
         link.download = "resume.pdf"; 
